894cc5a9479d8820102a1f629735eceaea282f0d
Koa <64676511+Tadashi-sudo@users.noreply.github.com>
2022-06-13 21:10:56 -1000

    Create main.py
    Brute forced so far

---
+++ main.py
@@ -0,0 +1,10 @@
+﻿def calc(*prices):
+    return 0
+
+dict = {}
+for each in range(1, 101):
+    dict.update({(1.57 * each) + 2.61 : each})
+
+for price, num in dict.items():
+    if float.is_integer(price) == True:
+        print(num, "$1.50 items must be bought")
