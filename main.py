6c7585a0bc5b86baf465bab676fd866011fc81f0
Koa <64676511+Tadashi-sudo@users.noreply.github.com>
2022-08-01 23:03:32 -1000

    Updated for 25 cents price increase

--- main.py
+++ main.py
@@ -1,10 +1,57 @@
-﻿def calc(*prices):
-    return 0
+﻿import math
+import kivy
+kivy.require("1.9.0")
+from kivy.app import App
+from kivy.uix.button import Label
 
-dict = {}
-for each in range(1, 101):
-    dict.update({(1.57 * each) + 2.61 : each})
+class welcome(App):
+    def build(self):
+        return Label(text = "Daiso Dollar Checker")
 
-for price, num in dict.items():
-    if float.is_integer(price) == True:
-        print(num, "$1.50 items must be bought")
+def truncate(x):
+    return math.floor(x * 10 ** 2) / 10 ** 2
+
+def calc(*snack, **drink):
+    dict = {}
+    tax = 0.047
+    snax = 0
+    drinx = 0
+    for each in snack:
+        for every in each:
+            snax += truncate((every * tax) + every)
+
+    for each in drink:
+        for every in each:
+            drinx += truncate((every * tax) + every + 6)
+
+    tot = snax + drinx
+
+    for each in range(1, 1001):
+        dict.update({(1.83 * each) + tot : each})
+
+    shown = False
+    for price, num in dict.items():
+        if float.is_integer(price) == True and shown == False:
+            print(num, "$1.75 items must be bought for a total of", price, "dollars")  
+            shown = True
+
+s_price = []
+s_total = input("Num of snack(s): ")
+s_total = int(s_total)
+for each in range(s_total):
+    temp = input("Price of snack: ")
+    temp = float(temp)
+    s_price.append(temp)
+
+d_price = []
+d_total = input("Num of drink(s): ")
+d_total = int(d_total)
+for each in range(d_total):
+    temp = input("Price of drink: ")
+    temp = float(temp)
+    d_price.append(temp)
+
+calc(s_price, d_price)
+
+run = welcome()
+run.run()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+
+
+    for each in range(1, 1001):
+        dict.update({(1.57 * each) + tot : each})
