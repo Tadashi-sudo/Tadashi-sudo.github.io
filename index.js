bbe2933d7de6eccfc4ba73455df9760b1e1f34fa
Koa <64676511+Tadashi-sudo@users.noreply.github.com>
2022-08-04 01:47:01 -1000

    Completed

---
+++ index.js
@@ -0,0 +1,22 @@
+﻿function trun(x) {
+    return Math.floor(x * 10 ** 2) / 10 ** 2;
+}
+
+function calc(snack, numDrink, drink) {
+    var tax = 0.047;
+    
+    snack = parseFloat(snack);
+    numDrink = parseInt(numDrink);
+    drink = parseFloat(drink);
+
+    snack += (snack * tax);
+    drink += (drink * tax) + (0.06 * numDrink);
+    total = snack + drink;
+
+    for (let i = 1000; i > 0; i--) {
+        cost = trun((1.83 * i) + total);
+        if (Number.isInteger(cost)) {
+            document.getElementById('output').innerHTML = i + " items for $" + cost;
+        }
+    }
+}
--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-
-
-    for each in range(1, 1001):
-        dict.update({(1.57 * each) + tot : each})
